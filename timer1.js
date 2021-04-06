475725cb67f50f3f8af9c5ed0440e6bb258bd36e
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-06 20:29:03 +0000

    timer1

--- timer1.js
+++ timer1.js
@@ -1,18 +1,22 @@
-if (process.argv.length <= 2) {
-  console.log("Missing alarm arguments");
-}
-else {
-  args = process.argv.splice(2);
-  // console.log(args)
-
-  for (let time of args) {
-    time = Number.parseInt(time)
-
-    if (time > 0 && Number.isInteger(time)) {
-      setTimeout(() => {
-        process.stdout.write('\x07');
-      }, time * 1000);
-    }
-
+const timer1 = (args) => {
+  if (args.length <= 2) {
+    console.log("Missing alarm arguments");
   }
-}
+  else {
+    args = args.splice(2);
+    // console.log(args)
+
+    for (let time of args) {
+      time = Number.parseInt(time)
+
+      if (time > 0 && Number.isInteger(time)) {
+        setTimeout(() => {
+          process.stdout.write('\x07');
+        }, time * 1000);
+      }
+
+    }
+  }
+};
+
+timer1(process.argv);
